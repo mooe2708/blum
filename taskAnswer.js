5640b51d9c0ca1781584790a5dc8ac7ac1c2ef04
hananto budi pratomo <33036408+mooe2708@users.noreply.github.com>
2024-10-07 21:37:20 +0700

    Update taskAnswer.js

--- taskAnswer.js
+++ taskAnswer.js
@@ -6,7 +6,13 @@ export class TASKANSWER {
       "53044aaf-a51f-4dfc-851a-ae2699a5f729": "HEYBLUM",
       "835d4d8a-f9af-4ff5-835e-a15d48e465e6": "CRYPTOBLUM",
       "3c048e58-6bb5-4cba-96cb-e564c046de58": "SUPERBLUM",
-      "350501e9-4fe4-4612-b899-b2daa11071fb": "CRYPTOSMART"
+      "350501e9-4fe4-4612-b899-b2daa11071fb": "CRYPTOSMART",
+      "92373c2b-2bf3-44c0-90f7-a7fd146c05c5": "HAPPYDOGS",
+      "b611352b-0d8c-44ec-8e0f-cd71b5922ca5": "BLUMERSSS",
+      "d2715289-b487-43bc-bc21-18224f8f6bc3": "NODOXXING",
+      "c60919cd-0282-46fe-854a-1da0a01db9b2": "blum - big city life",
+      "7067a3db-d9c5-4268-ac19-c393743e8491": "WOWBLUM",
+      "1572a605-d714-4f2c-8045-9c5f874d9c7e": "MEMEBLUM"
       // Add more task ids and their corresponding answers here
     };
   
